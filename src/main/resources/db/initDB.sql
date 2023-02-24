b641f1395f0bbdfba5e803ac13e02c6d3d098214
VladimirSafronov <safron5000@mail.ru>
2023-02-24 15:44:24 +0600

    Add JdbcMealRepository

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -27,11 +27,11 @@ CREATE TABLE user_role
 
 CREATE TABLE meals
 (
-    meal_id     INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
     date_time   TIMESTAMP           DEFAULT now() NOT NULL,
     description VARCHAR                           NOT NULL,
     calories    INTEGER             DEFAULT 0     NOT NULL,
     user_id     INTEGER                           NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
-CREATE UNIQUE INDEX meals_date_time_idx ON meals (date_time);
+CREATE UNIQUE INDEX meals_date_time_idx ON meals (user_id, date_time);
