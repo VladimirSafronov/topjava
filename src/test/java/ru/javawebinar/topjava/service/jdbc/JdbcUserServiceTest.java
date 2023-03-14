f7ec57fd7b846cda9a5752ec35141246835c55f9
VladimirSafronov <safron5000@mail.ru>
2023-03-14 19:53:05 +0300

    Add profiles jpa,datajpa,jdbc
     * add AbstractServiceTest

---
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava.service.jdbc;
+
+import static ru.javawebinar.topjava.Profiles.JDBC;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+
+@ActiveProfiles(JDBC)
+public class JdbcUserServiceTest extends AbstractUserServiceTest {
+
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava.service.jpa;
+
+import static ru.javawebinar.topjava.Profiles.JPA;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+
+@ActiveProfiles(JPA)
+public class JpaUserServiceTest extends AbstractUserServiceTest {
+
+}
