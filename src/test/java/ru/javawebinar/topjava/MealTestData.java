c9d69904a545705441fb56c8523b85ab22e7ce0e
VladimirSafronov <safron5000@mail.ru>
2023-04-03 16:18:02 +0300

    Implement MealRestController
    * add controller tests

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -5,13 +5,14 @@ import ru.javawebinar.topjava.model.Meal;
 import java.time.Month;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import ru.javawebinar.topjava.to.MealTo;
 
 import static java.time.LocalDateTime.of;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
 public class MealTestData {
     public static final MatcherFactory.Matcher<Meal> MEAL_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(Meal.class, "user");
-
+    public static final MatcherFactory.Matcher<MealTo> TO_MATCHER = MatcherFactory.usingEqualsComparator(MealTo.class);
     public static final int NOT_FOUND = 10;
     public static final int MEAL1_ID = START_SEQ + 3;
     public static final int ADMIN_MEAL_ID = START_SEQ + 10;
