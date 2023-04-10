99cd2f0d36353dc52b6ef1944fc69d93b040b1a1
VladimirSafronov <safron5000@mail.ru>
2023-04-10 21:37:55 +0600

    HW08
    * Add MealUIController

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -1,41 +1,33 @@
 let form;
-
 function makeEditable(datatableApi) {
     ctx.datatableApi = datatableApi;
     form = $('#detailsForm');
-    $(".delete").click(function () {
-        if (confirm('Are you sure?')) {
-            deleteRow($(this).closest('tr').attr("id"));
-        }
-    });
 
     $(document).ajaxError(function (event, jqXHR, options, jsExc) {
         failNoty(jqXHR);
     });
-
     // solve problem with cache in IE: https://stackoverflow.com/a/4303862/548473
     $.ajaxSetup({cache: false});
 }
-
 function add() {
     form.find(":input").val("");
     $("#editRow").modal();
 }
 
 function deleteRow(id) {
-    $.ajax({
-        url: ctx.ajaxUrl + id,
-        type: "DELETE"
-    }).done(function () {
-        updateTable();
-        successNoty("Deleted");
-    });
+    if (confirm('Are you sure?')) {
+        $.ajax({
+            url: ctx.ajaxUrl + id,
+            type: "DELETE"
+        }).done(function () {
+            ctx.updateTable();
+            successNoty("Deleted");
+        });
+    }
 }
 
-function updateTable() {
-    $.get(ctx.ajaxUrl, function (data) {
-        ctx.datatableApi.clear().rows.add(data).draw();
-    });
+function updateTableByData(data) {
+    ctx.datatableApi.clear().rows.add(data).draw();
 }
 
 function save() {
@@ -45,20 +37,17 @@ function save() {
         data: form.serialize()
     }).done(function () {
         $("#editRow").modal("hide");
-        updateTable();
+        ctx.updateTable();
         successNoty("Saved");
     });
 }
-
 let failedNote;
-
 function closeNoty() {
     if (failedNote) {
         failedNote.close();
         failedNote = undefined;
     }
 }
-
 function successNoty(text) {
     closeNoty();
     new Noty({
@@ -68,7 +57,6 @@ function successNoty(text) {
         timeout: 1000
     }).show();
 }
-
 function failNoty(jqXHR) {
     closeNoty();
     failedNote = new Noty({
